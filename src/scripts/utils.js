e8682c96b1c2746c0f8f772127a77c5a05dab1cc
Thamires <thamires@bizcommerce.com.br>
2019-10-15 15:24:49 -0300

    fix functions utils

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -33,16 +33,3 @@ function hasClass(el, selector) {
 
   return false;
 }
-
-// function hasClass(selector) {
-//   var className = " " + selector + " ",
-//       i = 0,
-//       l = this.length;
-//   for (; i < l; i++) {
-//       if (this[i].nodeType === 1 && (" " + this[i].className + " ").replace(rclass, " ").indexOf(className) >= 0) {
-//           return true;
-//       }
-//   }
-
-//   return false;
-// }

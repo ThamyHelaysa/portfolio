cf00f83a63bd8cd1a964663a442ea612fa019095
Thamires <thamires@bizcommerce.com.br>
2019-10-15 09:47:18 -0300

    prev next with arrow keys

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,26 +4,32 @@ const slider      = document.getElementById("slider_list"),
 
 var count = 1;
 
+// Go to the next slide
 function nextSlider(el){
-    
-    if (count < sliderItems){
-        slider.style.transform = `translateX( calc( ${count} * -100% + (${gap * count}px) ) )`
-        count++
-        console.log("next", count)
-    } else {
-        return false
-    }
+  if (count < sliderItems){
+    slider.style.transform = `translateX( calc( ${count} * -100% + (${gap * count}px) ) )`
+    count++
+  } else {
+    return false
+  }
 }
 
+// Return to the previous slide
 function prevSlider(el){
-    
-    if (count != 1){
-        count--
-        slider.style.transform = `translateX( calc( (${count} - 1) * -100% + (${gap * (count - 1)}px) ) )`
-        
-        
-        console.log("prev", count)
-    } else {
-        return false
-    }
+  if (count != 1){
+    count--
+    slider.style.transform = `translateX( calc( (${count} - 1) * -100% + (${gap * (count - 1)}px) ) )`
+  } else {
+    return false
+  }
 }
+
+
+document.addEventListener ('keydown', (event) => {
+  const keyName = event.key;
+  if (keyName === "ArrowRight"){
+    nextSlider();
+  } else if (keyName === "ArrowLeft"){
+    prevSlider();
+  }
+});

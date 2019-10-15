7a881bcb997ebfcb40a174e119cd9b083772676c
Thamires <thamires@bizcommerce.com.br>
2019-10-15 09:30:20 -0300

    prev next buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ const slider      = document.getElementById("slider_list"),
       sliderItems = document.getElementsByClassName("slider-item").length,
       gap         = 16
 
-var count = 0;
+var count = 1;
 
 function nextSlider(el){
     
@@ -17,9 +17,11 @@ function nextSlider(el){
 
 function prevSlider(el){
     
-    if (count > 0 && count <= sliderItems){
-        slider.style.transform = `translateX( calc( ${count} * -100% + (${gap * count}px) ) )`
+    if (count != 1){
         count--
+        slider.style.transform = `translateX( calc( (${count} - 1) * -100% + (${gap * (count - 1)}px) ) )`
+        
+        
         console.log("prev", count)
     } else {
         return false

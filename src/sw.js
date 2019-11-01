089253b39874d6938c0910c54f8f2b9d6b8ea5be
Thamires Helaysa <thamy.helaysa@live.com>
2019-11-01 20:23:36 -0300

    service worker fix

--- src/sw.js
+++ src/sw.js
@@ -2,15 +2,31 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '/index.html',
-       '/scripts/index.js',
-       '/scripts/router.js',
-       '/scripts/utils.js',
-       '/css/main.css',
-       '/css/variables.css',
-       '/media/dadaismo.png',
-       '/media/teste-imagem.jpg'
+      '/index.html',
+      '/scripts/index.js',
+      '/scripts/router.js',
+      '/scripts/utils.js',
+      '/css/main.css',
+      '/css/variables.css',
+      '/media/dadaismo.png',
+      '/media/teste-imagem.jpg'
      ]);
    })
  );
+});
+
+self.addEventListener('fetch', function(event) {
+
+  console.log(event.request.url);
+  
+  event.respondWith(
+  
+    caches.match(event.request).then(function(response) {
+    
+      return response || fetch(event.request);
+    
+    })
+  
+  );
+  
 });

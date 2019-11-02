3d3cc62e5433bb5dcac48ad340a6b5253512631b
Thamires Helaysa <thamy.helaysa@live.com>
2019-11-01 21:27:23 -0300

    manifest, ico, logo + more

--- src/sw.js
+++ src/sw.js
@@ -2,6 +2,8 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
+      '/proj1',
+      '/tcc',
       '/index.html',
       '/scripts/index.js',
       '/scripts/router.js',
@@ -9,7 +11,8 @@ self.addEventListener('install', function(e) {
       '/css/main.css',
       '/css/variables.css',
       '/media/dadaismo.png',
-      '/media/teste-imagem.jpg'
+      '/media/teste-imagem.jpg',
+      'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap'
      ]);
    })
  );

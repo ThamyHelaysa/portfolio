73592721805b914870135256f7d6eb9e07da19bb
Thamires H <thamires.santos@webjump.com.br>
2023-01-02 19:17:10 -0300

    feature: adding new line

--- thamy-blog/src/pages/cv.js
+++ thamy-blog/src/pages/cv.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 const Curriculum = () => {
     return (
         <Layout pageTitle='About Me'>
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <p></p>
         </Layout>
     )
 }

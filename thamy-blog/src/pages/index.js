9d76150497e1c85d8be3d8d83b5f8d38d280d3da
Thamires H <thamires.santos@webjump.com.br>
2023-01-02 18:46:02 -0300

    feature: adding raw components

---
+++ thamy-blog/src/pages/index.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Thamires Helaysa">
+      <p>
+        Aspiring front-end developer with a passion for creating intuitive and visually appealing user experiences. 
+        Skilled in HTML, CSS, and JavaScript, and always looking to learn and stay up-to-date with the latest technologies. 
+        Inspired by the mysterious and unsettling world of "The King in Yellow," seeking a position where I can use my skills 
+        to bring a touch of the unknown to the digital realm.
+      </p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
